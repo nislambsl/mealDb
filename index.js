a8d41b3c68ffb5a12284dbec558beaa11399ca0b
Md Nazrul Islam <nazrulbsl7@gmail.com>
2022-11-02 00:48:53 +0600

    Dynamic dataLoding updated

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const loadMeals = (search) => {
     .then(data => displayMeal(data.meals))
 }
 
-const displayMeal = (meals)=>{   
+const displayMeal = (meals)=>{  
+  console.log(meals) 
    const mealContainer = document.getElementById('meals-container');
    mealContainer.innerHTML = ``;
    for(const meal of meals){
  
    const mealDiv = document.createElement('div')
    mealDiv.classList.add("col")
-   mealDiv.innerHTML = `<div class="card">
-         <img src="${meal.strMealThumb
-   }" class="card-img-top" alt="...">
+   mealDiv.innerHTML = `<div onClick="loadMealDetails(${meal.idMeal})" class="card">
+         <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
          <div class="card-body">
            <h5 class="card-title">${meal.strMeal}</h5>
            <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
@@ -30,6 +30,27 @@ const displayMeal = (meals)=>{
     loadMeals(searchText);
     searchFeild.value = "";
     
+   }
+   const loadMealDetails = (idMeal) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showMealDetails(data.meals[0]))
+   }
+
+   const showMealDetails = meal => {
+    const mealDetailsContainer = document.getElementById('meal-details');
+    mealDetailsContainer.innerHTML = ``;
+    const mealDetailsDiv = document.createElement('div');
+    mealDetailsDiv.classList.add('card');
+    mealDetailsDiv.innerHTML = `<img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body text-center">
+      <h5 class="card-title text-warning"> Meal Title: ${meal.strMeal}</h5>
+      <h6 class="card-title text-info"> Meal Id: ${meal.idMeal}</h6>
+      <p class="card-text">${meal.strInstructions.slice(0, 250)}</p>
+      
+    </div>`;
+    mealDetailsContainer.appendChild(mealDetailsDiv);
    }
    loadMeals('');
    
